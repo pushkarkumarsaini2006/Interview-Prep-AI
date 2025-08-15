369f158897687416a00080242f6a269155d8995c
Pushkar Kumar Saini <pushkarkumarsaini2006@gmail.com>
2025-08-15 12:58:12 +0530

    Update footer text to 'Made by Pushkar Kumar Saini'

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -140,7 +140,7 @@ const LandingPage = () => {
         </div>
 
         <div className="text-sm bg-gray-50 text-secondary text-center p-5 mt-5">
-          Made with ❤️... Happy Coding
+          Made by Pushkar Kumar Saini
         </div>
       </div>
 
